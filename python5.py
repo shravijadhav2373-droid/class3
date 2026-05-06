ed7909ca3ef7e66f7aa72eac9c763ed0d34d6030
shravijadhav2373-droid <shravijadhav2373@gmail.com>
2026-05-06 22:20:31 +0530

    Implement bubble sort for user-input numbers

---
+++ python5.py
@@ -0,0 +1,8 @@
+numbers = list(map(int, input("Enter numbers: ").split()))
+
+for i in range(len(numbers)):
+    for j in range(0, len(numbers)-i-1):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+
+print("Sorted list:", numbers)
